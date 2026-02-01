0d269bb73642031830949a0f7ef652aa61c6ad76
Saransh <saransh@catering.local>
2026-02-01 21:59:25 +0530

    Fix paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/divya-catering/',
     build: {
         rollupOptions: {
             input: {
